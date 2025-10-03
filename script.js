0b12e1524bfc70355ecdc89ae6d272b61d7e1830
Bardahzin <gabriel.bardah2008@gmail.com>
2025-10-03 09:53:22 -0300

    finalização, adição de projetos da turma(incompleto)

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ window.addEventListener("resize", resize);
 resize();
 
 const particles = [];
-const PARTICLE_COUNT = 400;
-const MAX_DIST = 120;
+const PARTICLE_COUNT = 200;
+const MAX_DIST = 220;
 
 for (let i = 0; i < PARTICLE_COUNT; i++) {
   particles.push({
